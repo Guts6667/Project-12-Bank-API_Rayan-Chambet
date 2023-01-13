7ff30db91bfe4c8bbb5f4cd1875e0c2c62b1faa1
Rayan <rayanchambet1@gmail.com>
2023-01-13 17:54:56 +0100

    V.0.1.2.8
    Added error message in case of invalid values in signin form

--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -21,6 +21,9 @@ const SignIn = () => {
     const token = useSelector((state) => state.token)
     // Get the profile's state
     const userProfile = useSelector((state) => state.profile)
+    //States used to display error message in case of invalid values in the form
+    const [isInvalidEmail, setIsInvalidEmail] = useState(false);
+    const [isInvalidPassword, setIsInvalidPassword] = useState(false);
     // Retrieves the input values via onChange listener
     const [email, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -37,23 +40,33 @@ const SignIn = () => {
     // Status = 200, the token is saved in the state
       if(dataToken.status === 200){
         dispatch(tokenActions.getToken(dataToken.body.token))
-      }
-    // If the state in charge of the taken is true
-      if(token){
-    // Update the auth state to true
-        dispatch(authActions.login())
-    // Calls and save the user datas in the const dataUser
-        const dataUser = await login(dataToken.body.token);
-        if(dataUser.status === 200){
-          dispatch(profileActions.getNames({firstName : dataUser.body.firstName, lastName : dataUser.body.lastName}))
-          if(userProfile){
-            navigate(`/profile/${dataUser.body.id}`)
+         // If the state in charge of the taken is true
+      if(token){;
+        // Update the auth state to true
+            dispatch(authActions.login())
+        // Calls and save the user datas in the const dataUser
+            const dataUser = await login(dataToken.body.token);
+
+            if(dataUser.status === 200){
+              dispatch(profileActions.getNames({firstName : dataUser.body.firstName, lastName : dataUser.body.lastName}))
+              if(userProfile){
+                navigate(`/profile/${dataUser.body.id}`)
+              }
+              else{
+                navigate("/*")
+              }
+            }
           }
-          else{
-            navigate("/*")
-          }
+          
+      }else{
+        if(dataToken.message === "Error: User not found!"){
+          setIsInvalidEmail(true)
+        }
+        if(dataToken.message === "Error: Password is invalid"){
+          setIsInvalidPassword(true)
         }
       }
+   
 
       }
     return(
@@ -65,10 +78,12 @@ const SignIn = () => {
                     <div className="form-signIn-input">
                         <label htmlFor="">Username</label>
                         <input type={"text"} id="username" onChange={(e)=> setUsername(e.target.value)}/>
+                        {isInvalidEmail && <span className="error">Error: User not found!</span>}
                     </div>
                     <div className="form-signIn-input">
                         <label htmlFor="">Password</label>
                         <input type={"password"} required id="password" onChange={(e)=> setPassword(e.target.value)}/>
+                        {isInvalidPassword && <span className="error">Error: Password is invalid</span>}
                     </div>
                     <div className="form-signIn-ratio" >
                         <label htmlFor="">Remember me</label>
